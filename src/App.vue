7ba99d3ac0b5a1dae3999b2d42631dbc440f493d
flood <kansini@gmail.com>
2019-12-16 15:40:38 +0800

    delete router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <schedule/>
+    </div>
 </template>
+<script>
+    import schedule from '@/components/schedule'
 
+    export default {
+        components: {
+            schedule
+        }
+    }
+</script>
 <style lang="scss">
-#app {
-
-}
+    #app {
+        width: 100%;
+        padding: 16px;
+        box-sizing: border-box;
+    }
 
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -13,7 +12,7 @@ Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  //router,
   store,
   render: h => h(App)
 }).$mount('#app')
